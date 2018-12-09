5ecbac8d8df9b2a81ef5663aa731e7104da12a83
unknown <jovo.skorupan@gmail.com>
2018-12-09 10:05:58 +0100

    clean repo

--- service-information-bot/service-information-bot/src/com/jovvo/sibot/openweathermap/OpenWeatherMapClient.java
+++
@@ -1,9 +0,0 @@
-package com.jovvo.sibot.openweathermap;
-
-import org.springframework.http.ResponseEntity;
-
-public interface OpenWeatherMapClient {
-	public ResponseEntity<OpenWeatherForecastResponse> fetchForecast(String city);
-	
-
-}
